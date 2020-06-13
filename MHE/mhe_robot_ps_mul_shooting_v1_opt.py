6ba379222c9bd3dc212a08cdcab58d9f9955f211
tomcattigerkkk <wei.luo.88@outlook.com>
2020-06-13 21:52:32 +0200

    MHE v1 opt version finish

--- MHE/mhe_robot_ps_mul_shooting_v1_opt.py
+++ MHE/mhe_robot_ps_mul_shooting_v1_opt.py
@@ -4,7 +4,7 @@
 import casadi as ca
 import numpy as np
 
-from draw import Draw_MPC_point_stabilization_v1, draw_gt, draw_gt_measurements, draw_gtmeas_noisemeas
+from draw import Draw_MPC_point_stabilization_v1, draw_gt, draw_gt_measurements, draw_gtmeas_noisemeas, draw_gt_mhe_measurements
 
 def shift_movement(T, t0, x0, u, x_n, f):
     f_value = f(x0, u[0])
@@ -23,10 +23,10 @@ def prediction_state(x0, u, T, N):
         states[i+1, 1] = states[i, 1] + u[i, 0] * np.sin(states[i, 2]) * T
         states[i+1, 2] = states[i, 2] + u[i, 1] * T
     return states
-    
+
 
 if __name__ == '__main__':
-    T = 0.2 
+    T = 0.2
     N = 10
     rob_diam = 0.3 # [m]
     v_max = 0.6
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     # create model
     f = lambda x_, u_: ca.vertcat(*[u_[0]*np.cos(x_[2]), u_[0]*np.sin(x_[2]), u_[1]])
     f_np = lambda x_, u_: np.array([u_[0]*np.cos(x_[2]), u_[0]*np.sin(x_[2]), u_[1]])
-    
+
     ## init_condition
     opti.subject_to(opt_states[0, :] == opt_x0.T)
     for i in range(N):
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         obj = obj + ca.mtimes([(opt_states[i, :]-opt_xs.T), Q, (opt_states[i, :]-opt_xs.T).T]) + ca.mtimes([opt_controls[i, :], R, opt_controls[i, :].T])
 
     opti.minimize(obj)
-    
+
     #### boundrary and control conditions
     opti.subject_to(opti.bounded(-2.0, x, 2.0))
     opti.subject_to(opti.bounded(-2.0, y, 2.0))
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     mpciter = 0
 
     while(np.linalg.norm(current_state-final_state)>0.05 and mpciter-sim_time/T<0.0  ):
-        opti.set_value(opt_x0, current_state) 
+        opti.set_value(opt_x0, current_state)
         opti.set_initial(opt_controls, next_controls.reshape(N, n_controls))# (N, 2)
         opti.set_initial(opt_states, next_states) # (N+1, 3)
         ## solve the problem once again
@@ -114,7 +114,7 @@ if __name__ == '__main__':
         t0, current_state, u0, next_states = shift_movement(T, t0, current_state, u_res, x_m, f_np)
         xx.append(current_state)
         mpciter = mpciter + 1
-    
+
     xx_np = np.array(xx)
     # draw_result = Draw_MPC_point_stabilization_v1(rob_diam=0.3, init_state=init_state, target_state=final_state,robot_states=xx)
     # draw_gt(t_c, xx_np)
@@ -134,12 +134,12 @@ if __name__ == '__main__':
     # draw_gtmeas_noisemeas(t_c, xx_np, y_measurements)
 
 
-    ## MHE 
+    ## MHE
     ### now we only know the control u_c and y_measurements
     mhe_opt = ca.Opti() # new optimization class
     u_c_np = np.array(u_c)
     T_mhe = 0.2
-    N_MHE = y_measurements.shape[0] - 1 # estimation horizon 
+    N_MHE = y_measurements.shape[0] - 1 # estimation horizon
     print("MHE horizon {}".format(N_MHE))
     ### using the same model and states （states, f, next_controls)
     mhe_states = mhe_opt.variable(N_MHE+1, n_states)
@@ -151,23 +151,23 @@ if __name__ == '__main__':
     mhe_omega = mhe_controls[:, 1]
     mhe_mes_ref = mhe_opt.parameter(N_MHE+1, 2)
     mhe_u_ref = mhe_opt.parameter(N_MHE, 2)
-    ## measurement model  
+    ## measurement model
     f_m = lambda x, y: ca.vertcat(*[np.sqrt(x**2+y**2), np.arctan(y/x)])
     V_mat = np.linalg.inv(np.sqrt(meas_cov))
     W_mat = np.linalg.inv(np.sqrt(con_cov))
-    
+
     obj_mhe = 0
     for i in range(N_MHE+1):
         h_x = f_m(mhe_states[i, 0], mhe_states[i, 1])
-        temp_diff_ = mhe_mes_ref[i] - h_x
-        obj_mhe = obj_mhe + ca.mtimes([temp_diff_.T, V_mat, temp_diff_])
+        temp_diff_ = mhe_mes_ref[i, :] - h_x.T
+        obj_mhe = obj_mhe + ca.mtimes([temp_diff_, V_mat, temp_diff_.T])
 
     for i in range(N_MHE):
         temp_diff_ = mhe_u_ref[i, :] - mhe_controls[i, :]
         obj_mhe = obj_mhe + ca.mtimes([temp_diff_, W_mat, temp_diff_.T])
 
     mhe_opt.minimize(obj_mhe)
-    
+
     ## multiple shooting constraints
     for i in range(N_MHE):
         x_next_ = mhe_states[i, :] + f(mhe_states[i, :], mhe_controls[i, :]).T*T
@@ -187,13 +187,14 @@ if __name__ == '__main__':
     ### initial states from the measured range and bearning
     init_state_m = np.zeros((N_MHE+1, n_states))
     for i in range(N_MHE+1):
-        init_state_m[i] = np.array([y_measurements[i, 0]*np.cos(y_measurements[i, 1]), 
-                                    y_measurements[i, 0]*np.sin(y_measurements[i, 1]), 
+        init_state_m[i] = np.array([y_measurements[i, 0]*np.cos(y_measurements[i, 1]),
+                                    y_measurements[i, 0]*np.sin(y_measurements[i, 1]),
                                     0.0])
     mhe_opt.set_initial(mhe_states, init_state_m)
-    mhe_opt.set_initial(mhe_controls, np.array(u_c[:-1]))# (N, 2)
+    mhe_opt.set_initial(mhe_controls, np.array(u_c[:N_MHE]))# (N, 2)
     ## solve the MHE
     sol = mhe_opt.solve()
-        ## obtain the control input
-        #u_res = sol.value(opt_controls)
-        #x_m = sol.value(opt_states)
+    ## obtain the control input
+    u_sol = sol.value(mhe_controls)
+    state_sol = sol.value(mhe_states)
+    draw_gt_mhe_measurements(t_c, xx_np, y_measurements, state_sol)
