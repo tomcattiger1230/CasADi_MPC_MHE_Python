9eff27f950d4fcf9179d9ba69d40dfd9e64f8885
tomcattigerkkk <wei.luo.88@outlook.com>
2020-06-19 23:25:59 +0200

    small

--- MPC/sim_2_mpc_mul_shooting.py
+++ MPC/sim_2_mpc_mul_shooting.py
@@ -9,7 +9,7 @@ import time
 from draw import Draw_MPC_point_stabilization_v1
 
 def shift_movement(T, t0, x0, u, x_f, f):
-    f_value = f(x0, u[:, 0])
+    f_value = f(x0, u[0, :])
     st = x0 + T*f_value.full()
     t = t0 + T
     u_end = np.concatenate((u[:, 1:], u[:, -1:]), axis=1)
@@ -74,6 +74,7 @@ if __name__ == '__main__':
     ubg = 0.0
     lbx = []
     ubx = []
+
     for _ in range(N):
         lbx.append(-v_max)
         lbx.append(-omega_max)
@@ -92,12 +93,12 @@ if __name__ == '__main__':
     x0 = np.array([0.0, 0.0, 0.0]).reshape(-1, 1)# initial state
     x0_ = x0.copy()
     x_m = np.zeros((n_states, N+1))
-    next_states = x_m.copy()
+    next_states = x_m.copy().T
     xs = np.array([1.5, 1.5, 0.0]).reshape(-1, 1) # final state
     u0 = np.array([1,2]*N).reshape(-1, 2).T# np.ones((N, 2)) # controls
     x_c = [] # contains for the history of the state
     u_c = []
-    t_c = [t0] # for the time
+    t_c = [] # for the time
     xx = []
     sim_time = 20.0
 
@@ -109,15 +110,15 @@ if __name__ == '__main__':
     while(np.linalg.norm(x0-xs)>1e-2 and mpciter-sim_time/T<0.0 ):
         ## set parameter
         c_p = np.concatenate((x0, xs))
-        init_control = np.concatenate((u0.T.reshape(-1, 1), next_states.T.reshape(-1, 1)))
+        init_control = np.concatenate((u0.reshape(-1, 1), next_states.reshape(-1, 1)))
         t_ = time.time()
         res = solver(x0=init_control, p=c_p, lbg=lbg, lbx=lbx, ubg=ubg, ubx=ubx)
         index_t.append(time.time()- t_)
         estimated_opt = res['x'].full() # the feedback is in the series [u0, x0, u1, x1, ...]
-        u0 = estimated_opt[:200].reshape(N, n_controls).T # (n_controls, N)
-        x_m = estimated_opt[200:].reshape(N+1, n_states).T# [n_states, N]
+        u0 = estimated_opt[:200].reshape(N, n_controls) # (N, n_controls)
+        x_m = estimated_opt[200:].reshape(N+1, n_states) # (N+1, n_states)
         x_c.append(x_m.T)
-        u_c.append(u0[:, 0])
+        u_c.append(u0[0, :])
         t_c.append(t0)
         t0, x0, u0, next_states = shift_movement(T, t0, x0, u0, x_m, f)
         x0 = ca.reshape(x0, -1, 1)
